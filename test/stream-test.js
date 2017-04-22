2bbdefe2dd576ebdb45a7eab9270a63ae8b0fe2d
Fedor Indutny <fedor@indutny.com>
2017-04-22 16:02:52 -0400

    test: test handshake

--- test/stream-test.js
+++ test/stream-test.js
@@ -8,13 +8,21 @@ const protocol = require('../');
 const Stream = protocol.Stream;
 
 describe('Stream', () => {
-  it('should handshake', () => {
+  it('should handshake', (cb) => {
     const keyPair = signatures.keyPair();
     const privateKey = keyPair.secretKey;
     const publicKey = keyPair.publicKey;
 
     const a = new Stream({ feedKey: publicKey, privateKey });
     const b = new Stream({ feedKey: publicKey, privateKey });
+
+    let waiting = 2;
+    function onSecure() {
+      if (--waiting === 0)
+        return cb();
+    }
+    a.on('secure', onSecure);
+    b.on('secure', onSecure);
 
     a.pipe(b);
     b.pipe(a);
